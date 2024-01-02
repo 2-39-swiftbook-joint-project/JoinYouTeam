70ce305bb9fc1e9db04b7f298618fc37736a14f6
ns.moskalev <nikmosyl@gmail.com>
2024-01-03 01:19:18 +0400

    add Creator Model

---
+++ JoinYouTeam/Models/Creator.swift
@@ -0,0 +1,28 @@
+//
+//  Creator.swift
+//  JoinYouTeam
+//
+//  Created by nikita on 03.01.24.
+//
+
+struct Creator {
+    let name: String
+    let githubAccount: String
+    let about: String
+    
+    static func getCreatorsList() -> [Creator] {
+         [
+            Creator(
+                name: "Nikita",
+                githubAccount: "@nikmosyl",
+                about: "Creator Model, AboutMe Screen"
+            ),
+            Creator(
+                name: "your name",
+                githubAccount: "your github account",
+                about: "your tasks"
+            )
+         ]
+        
+    }
+}
