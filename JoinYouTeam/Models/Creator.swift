8eaf71393839a43aba2f80e2f6cd9780c0376b38
ns.moskalev <nikmosyl@gmail.com>
2024-01-22 01:00:18 +0400

    Add Developer and Team models, add DataStore

--- JoinYouTeam/Models/Creator.swift
+++ JoinYouTeam/Models/Creator.swift
@@ -9,20 +9,4 @@ struct Creator {
     let name: String
     let githubAccount: String
     let about: String
-    
-    static func getCreatorsList() -> [Creator] {
-         [
-            Creator(
-                name: "Nikita",
-                githubAccount: "@nikmosyl",
-                about: "Creator Model, AboutUs Screen, SceneDelegate"
-            ),
-            Creator(
-                name: "your name",
-                githubAccount: "your github account",
-                about: "your tasks"
-            )
-         ]
-        
-    }
 }
