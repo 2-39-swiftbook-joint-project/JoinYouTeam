0b3eb7f62f1b134bd39a10e44510ae4f6726171f
Xenia Zagareva <xeniazagareva@gmail.com>
2024-01-22 03:22:32 +0300

    Start of work on the selection menu, team selection menu, team creation menu and my team meny

---
+++ JoinYouTeam/ViewControllers/SelectionMenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectionMenuViewController.swift
+//  JoinYouTeam
+//
+//  Created by Ксения Загарьева on 22.01.2024.
+//
+
+import UIKit
+
+class SelectionMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
