bc3ff49c91090821183c0ffa47c80ba4e2f2178d
ns.moskalev <nikmosyl@gmail.com>
2024-01-22 18:42:37 +0400

    Contribute commit

--- JoinYouTeam/ViewControllers/TestViewController.swift
+++ JoinYouTeam/ViewControllers/TestViewController.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class TestViewController: UIViewController {
 
+    @IBOutlet var userLabel: UILabel!
+    
+    let developers = StorageManager.shared.getDevelopers()
+    let teams = StorageManager.shared.getTeams()
+    
+    var user: Developer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        userLabel.text = "\(user.name) \(user.github) \(user.team?.name ?? "")"
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func loggerButton(_ sender: Any) {
+        let developers = StorageManager.shared.getDevelopers()
+        let teams = StorageManager.shared.getTeams()
+        print("\n\n----DATA LOG-----")
+        for developer in developers {
+            print(developer.name, developer.github, developer.team?.name)
+        }
+        for team in teams {
+            print(team.name, team.developers)
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
